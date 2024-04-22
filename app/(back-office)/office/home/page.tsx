23ab7049e9429378b4e3d4b8b9d54737bb5324c6
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2024-04-23 01:23:43 +0700

    `home` create redirect function if url on home

--- app/(back-office)/office/home/page.tsx
+++ app/(back-office)/office/home/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { push } = useRouter();
+
+  useEffect(() => {
+    push("/office/home/inventory-dashboard");
+  }, [push]);
+  return <div></div>;
 };
 
 export default HomePage;
